46fb4da59c7e82fcb87e91f301ee8f104bbf8bc7
Meyssam Molaei <34935149+meyssammolaye@users.noreply.github.com>
2025-07-30 11:57:59 +0200

    v1

--- scripts/prepare_dirs.sh
+++ scripts/prepare_dirs.sh
@@ -5,7 +5,13 @@ mkdir -p ./docker/mssql/data
 mkdir -p ./docker/airflow/dags
 mkdir -p ./docker/airflow/data
 
+sudo chown -R 10001:0 ./docker/mssql/data
+sudo chown -R 10001:0 ./docker/postgres/data
+sudo chown -R 10001:0 ./docker/airflow/dags
+sudo chown -R 10001:0 ./docker/airflow/data
+
+
 touch ./docker/postgres/init.sql
 touch ./docker/airflow/dags/transfer_dag.py
-
+docker rm -f $(docker ps -a) -q || true
 echo "Folders and placeholder files created."
